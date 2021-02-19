a0e21a35435f986e260222f134f35e5ae26e675f
Anthony-Rodriguez <7Anthony.Rodriguez7@gmail.com>
2021-02-19 11:42:05 -0500

    Add form and route for creating artifact
    - Made a form to create artifact. Haven't tested yet
    - Added route for form

--- src/components/Form/ArtifactForm.js
+++ src/components/Form/ArtifactForm.js
@@ -15,6 +15,66 @@ const ArtifactForm = ({ handleSubmit, handleInputChange, artifact }) => (
         onChange={handleInputChange}
       />
     </Form.Group>
+    <Form.Group controlId="category">
+      <Form.Label>Category</Form.Label>
+      <Form.Control
+        required
+        name="category"
+        as="select"
+        value={artifact.category}
+        onChange={handleInputChange}
+      >
+        <option value="1">Armor</option>
+        <option value="2">Potion</option>
+        <option value="3">Ring</option>
+        <option value="4">Rod</option>
+        <option value="5">Scroll</option>
+        <option value="6">Staff</option>
+        <option value="7">Wand</option>
+        <option value="8">Weapon</option>
+        <option value="9">Wondrous Item</option>
+      </Form.Control>
+    </Form.Group>
+    <Form.Group controlId="rarity">
+      <Form.Label>Rarity</Form.Label>
+      <Form.Control
+        required
+        name="rarity"
+        as="select"
+        value={artifact.rarity}
+        onChange={handleInputChange}
+      >
+        <option value="1">Common</option>
+        <option value="2">Uncommon</option>
+        <option value="3">Rare</option>
+        <option value="4">Very Rare</option>
+        <option value="5">Legendary</option>
+        <option value="6">Artifact</option>
+        <option value="7">Unknown Rarity</option>
+      </Form.Control>
+    </Form.Group>
+    <Form.Group controlId="attunement">
+      <Form.Label>Attunement Required</Form.Label>
+      <Form.Check
+        required
+        type="checkbox"
+        id="attunement-check"
+        value={artifact.attunement}
+        onChange={handleInputChange}
+      />
+    </Form.Group>
+    <Form.Group controlId="description">
+      <Form.Label>Description</Form.Label>
+      <Form.Control
+        required
+        name="description"
+        as="textarea"
+        rows={3}
+        placeholder="Artifact Description"
+        value={artifact.description}
+        onChange={handleInputChange}
+      />
+    </Form.Group>
     <Button
       variant="primary"
       type="submit"
---
+++ src/components/Routes/CreateArtifact.js
@@ -0,0 +1,85 @@
+import React, { Component, Fragment } from 'react'
+import { Redirect } from 'react-router-dom'
+import axios from 'axios'
+import apiUrl from '../../apiConfig'
+import ArtifactForm from '../Form/ArtifactForm'
+
+class CreateArtifact extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      artifact: {
+        name: '',
+        category: '',
+        rarity: '',
+        attunement: null,
+        description: ''
+      },
+      createdId: null
+    }
+  }
+  handleInputChange = event => {
+    event.persist()
+    const target = event.target
+    const value = target.type === 'checkbox' ? target.checked : target.value
+    const name = target.name
+    // const updatedField = {
+    //   [target.name]: target.value
+    // }
+    this.setState(() => {
+      const newArtifact = Object.assign({}, this.state.artifact, ([name]: value))
+      return { artifact: newArtifact }
+    })
+  }
+  handleSubmit = (event, UpdatedField) => {
+    event.preventDefault()
+    const { user, msgAlert } = this.props
+    const { artifact } = this.state
+    axios({
+      method: 'post',
+      url: apiUrl + '/artifacts/',
+      headers: {
+        'Authorization': `Token ${user.token}`
+      },
+      data: { artifact }
+    })
+      .then((res, msgAlert) => {
+        this.setState({ createdId: res.data.artifact.id })
+        return res
+      })
+      .then(() => msgAlert({
+        heading: 'Artifact Created Successfully',
+        message: `${artifact.name} has been added!`,
+        variant: 'success'
+      }))
+      .catch(error => {
+        msgAlert({
+          heading: 'Unfortunately, your powers have failed you.',
+          message: 'Here\'s why' + error.message,
+          variant: 'danger'
+        })
+      })
+  }
+  render () {
+    if (this.state.createdId) {
+      return <Redirect to ={`/artifacts/${this.state.createdId}/`}/>
+    }
+    return (
+      <Fragment>
+        <div className="row">
+          <div className="col-sm-10 col-md-8 mx-auto mt-5">
+            <h2>Create an Artifact</h2>
+            <ArtifactForm
+              artifact={this.state.artifact}
+              handleSubmit={this.handleSubmit}
+              handleInputChange={this.handleInputChange}
+            />
+          </div>
+        </div>
+      </Fragment>
+    )
+  }
+}
+
+export default CreateArtifact
